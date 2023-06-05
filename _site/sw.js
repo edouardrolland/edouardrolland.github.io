985aaeabd8580c34363dadd320dc6347e873b906
Edouard Rolland <65714311+edouardrolland@users.noreply.github.com>
2023-06-05 12:27:25 +0100

    Add files via upload

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230605.115634';
+const cacheName = 'chirpy-20230605.132130';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
