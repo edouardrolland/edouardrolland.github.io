2ef5c7d17045ee301291ba7d77d8a1193a31e74b
Edouard <edouard.rlld@gmail.com>
2023-06-05 12:34:18 +0100

    Commit Test 2

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230605.132130';
+const cacheName = 'chirpy-20230605.133006';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
