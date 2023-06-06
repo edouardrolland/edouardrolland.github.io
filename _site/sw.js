d6859777671859100b860d4bd2d165595b968706
Edouard <edouard.rlld@gmail.com>
2023-06-06 15:22:32 +0100

    Rédaction d'un deuxième article :     - Résolution du problème de fichier bib     - Présentation du MSc.

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230606.155636';
+const cacheName = 'chirpy-20230606.162149';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
